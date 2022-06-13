10ca62783fcef08c3fb92de47246975e5d984bdc
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-13 20:04:22 +0700

    15-Repos-Setup

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 
 const Followers = () => {
   const { followers } = React.useContext(GithubContext);
-  console.log(followers);
+  //console.log(followers);
 
   return (
     <Wrapper>
       <div className="followers">
-        {followers.map((followers, index) => {
-          const { avatar_url: img, html_url, login } = followers;
+        {followers.map((follower, index) => {
+          const { avatar_url: img, html_url, login } = follower;
           return (
             <article key={index}>
               <img src={img} alt={login} />
