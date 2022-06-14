27c4c0fd95043bcb27d953d1041ec009194635aa
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-14 11:15:43 +0700

    23-Fusion-Chart-Doughnut-Chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -50,6 +50,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={languages} />
+        <div></div>
+        <Doughnut2D data={chartData} />
+        <div></div>
         {/* <ExampleChart dataProp={chartData} /> */}
       </Wrapper>
     </section>
