23b826b2bc5180c5fda2f7e9dcd084c6fa0d2161
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-15 18:00:11 +0700

    38-Wrapper

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,15 +3,14 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children, ...rest }) => {
-  //children is dashboard
-  const { isAuthenticated, user } = useAuth0;
+  const { isAuthenticated, user } = useAuth0();
   const isUser = isAuthenticated && user;
 
   return (
     <Route
       {...rest}
       render={() => {
-        return isUser ? children : <Redirect to="login"></Redirect>;
+        return isUser ? children : <Redirect to="/login"></Redirect>;
       }}
     ></Route>
   );
